04bcd68b60a78d20888bf8249e82cf772ae27e88
firmin nagnduli <finganduli@gmail.com>
2024-07-02 12:34:12 +0200

    fix: added dockfile

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.get("/api/hello", async (req, res) => {
       req.socket.remoteAddress ||
       "";
     const visitor = visitorName.slice(1, visitorName.length - 1);
-    const ipApiResponse = await axios.get(`http://ip-api.com/json/${clientIp}`);
+    const ipApiResponse = await fetch.get(`http://ip-api.com/json/${clientIp}`);
     const { city } = ipApiResponse.data || "new york";
 
     const weatherApiKey = process.env.WEATHERAPIKEY;
-    const weatherApiResponse = await axios.get(
+    const weatherApiResponse = await fetch.get(
       `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weatherApiKey}`
     );
     const temperature = weatherApiResponse.data.main.temp;
