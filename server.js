38d6a062ccf4f0f0c5ad826d619905c7f2c2219c
firmin nagnduli <finganduli@gmail.com>
2024-07-02 12:27:41 +0200

    fix: added dockfile

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import axios from "axios";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.get("/api/hello", async (req, res) => {
   try {
     const visitorName = req.query.visitor_name;
