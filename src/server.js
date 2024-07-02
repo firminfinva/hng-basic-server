7bc9052e7de58e36d0eb0ca0ef7734c6ef1238ea
firmin nagnduli <finganduli@gmail.com>
2024-07-02 10:08:47 +0200

    first commit

--- src/server.js
+++ src/server.js
@@ -4,28 +4,32 @@ import axios from "axios";
 const app = express();
 const port = process.env.PORT;
 app.get("/api/hello", async (req, res) => {
-  const visitorName = req.query.visitor_name;
-  const clientIp =
-    req.headers["cf-connecting-ip"] ||
-    req.headers["x-real-ip"] ||
-    req.headers["x-forwarded-for"] ||
-    req.socket.remoteAddress ||
-    "";
-  const visitor = visitorName.slice(1, visitorName.length - 1);
-  const ipApiResponse = await axios.get(`http://ip-api.com/json/${clientIp}`);
-  const { city } = ipApiResponse.data || "new york";
+  try {
+    const visitorName = req.query.visitor_name;
+    const clientIp =
+      req.headers["cf-connecting-ip"] ||
+      req.headers["x-real-ip"] ||
+      req.headers["x-forwarded-for"] ||
+      req.socket.remoteAddress ||
+      "";
+    const visitor = visitorName.slice(1, visitorName.length - 1);
+    const ipApiResponse = await axios.get(`http://ip-api.com/json/${clientIp}`);
+    const { city } = ipApiResponse.data || "new york";
 
-  const weatherApiKey = process.env.WEATHERAPIKEY;
-  const weatherApiResponse = await axios.get(
-    `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weatherApiKey}`
-  );
-  const temperature = weatherApiResponse.data.main.temp;
+    const weatherApiKey = process.env.WEATHERAPIKEY;
+    const weatherApiResponse = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weatherApiKey}`
+    );
+    const temperature = weatherApiResponse.data.main.temp;
 
-  res.json({
-    client_ip: clientIp,
-    location: city,
-    greeting: `Hello, ${visitor}!, the temperature is ${temperature}degrees Celsius in ${city}`,
-  });
+    res.json({
+      client_ip: clientIp,
+      location: city,
+      greeting: `Hello, ${visitor}!, the temperature is ${temperature}degrees Celsius in ${city}`,
+    });
+  } catch {
+    res.status(500).json({ error: error });
+  }
 });
 
 app.get("/", (req, res) => {
