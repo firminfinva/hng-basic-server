e93f1958f21e91a89973125bfcd0b309e59bad0e
firmin nagnduli <finganduli@gmail.com>
2024-07-02 09:51:11 +0200

    first commit

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import axios from "axios";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.get("/api/hello", async (req, res) => {
   const visitorName = req.query.visitor_name;
@@ -13,36 +13,21 @@ app.get("/api/hello", async (req, res) => {
     req.socket.remoteAddress ||
     "";
 
-  // let ip = req.socket.remoteAddress;
-  // ip = ip.split(":")[3];
-  const visitor = visitorName.slice(1, visitorName.length - 1);
   const ipApiResponse = await axios.get(`http://ip-api.com/json/${clientIp}`);
   console.log("ip Api", ipApiResponse);
   const { city } = ipApiResponse.data || "new york";
 
+  const weatherApiKey = process.env.WEATHERAPIKEY;
+  const weatherApiResponse = await axios.get(
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weatherApiKey}`
+  );
+  const temperature = weatherApiResponse.data.main.temp;
+
   res.json({
     client_ip: clientIp,
     location: city,
-    greeting: `Hello, ${visitor}!, the temperature is 11 degrees Celsius in ${city}`,
+    greeting: `Hello, ${visitor}!, the temperature is ${temperature}degrees Celsius in ${city}`,
   });
-  //   try {
-  //     const ipApiResponse = await axios.get(`http://ip-api.com/json/${clientIp}`);
-  //     const { city } = ipApiResponse.data;
-
-  //     const weatherApiKey = "YOUR_WEATHER_API_KEY";
-  //     const weatherApiResponse = await axios.get(
-  //       `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weatherApiKey}`
-  //     );
-  //     const temperature = weatherApiResponse.data.main.temp;
-
-  //     res.json({
-  //       client_ip: clientIp,
-  //       location: city,
-  //       greeting: `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${city}`,
-  //     });
-  //   } catch (error) {
-  //     res.status(500).json({ error: "Error fetching data" });
-  //   }
 });
 
 app.listen(port, () => {
